2d894ea4501d9c7769bf72cf499c11a60d5d0d60
schen22 <sarahchen22@gmail.com>
2019-06-03 01:30:16 -0700

    add empty credentials file

---
+++ lib/credentials.dart
@@ -0,0 +1,15 @@
+class Credentials {
+  String clientId;
+  String clientSecret;
+  String userAgent;
+  String username;
+  String password;
+
+  Credentials({
+    this.clientId,
+    this.clientSecret,
+    this.userAgent,
+    this.username,
+    this.password
+  });
+}
