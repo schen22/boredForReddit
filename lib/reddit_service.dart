c0fe9475c48080ccf58df16504a4193f32555276
schen22 <sarahchen22@gmail.com>
2019-06-06 22:57:56 -0700

    update service lulz

--- lib/reddit_service.dart
+++ lib/reddit_service.dart
@@ -5,42 +5,19 @@ import 'generated/post.pb.dart' as RedditData;
 
 import 'credentials.dart';
 
-Future<void> main() async {
-  Credentials c = new Credentials();
-  // Create the `Reddit` instance and authenticated
-  Reddit reddit = await Reddit.createScriptInstance(
-    clientId: c.clientId,
-    clientSecret: c.clientSecret,
-    userAgent: c.userAgent,
-    username: c.username,
-    password: c.password,
-  );
-
-  // Retrieve information for the currently authenticated user
-  Redditor currentUser = await reddit.user.me();
-  // Outputs: My name is DRAWApiOfficial
-  print("My name is ${currentUser.displayName}");
-
-  RedditService service = RedditService();
-  Submission post = await service.getWholesomeMeme();
-  String title = post.title;
-  print ('title = $title');
-  String url = service.getMemeSource(post);
-  print ('url = $url'); 
-}
-
 class RedditService {
-
-Future<Submission> getWholesomeMeme() async {
+  Future<Submission> getWholesomeMeme() async {
+    Credentials c = new Credentials();
+    // Create the `Reddit` instance and authenticated
     Reddit reddit = await Reddit.createScriptInstance(
-    clientId: 'xPa9bqSAzCHW_Q',
-    clientSecret: 'ImE9uvmF1czWGhhCOHFUKW03Vtc',
-    userAgent: 'boredforreddit',
-    username: "fredfishz",
-    password: "comets", // Fake
-  );
-
-  var wholesomeMemes = reddit.subreddit('wholesomememes');
+      clientId: c.clientId,
+      clientSecret: c.clientSecret,
+      userAgent: c.userAgent,
+      username: c.username,
+      password: c.password,
+    );
+    
+    var wholesomeMemes = reddit.subreddit('wholesomememes');
     try {
       SubmissionRef postRef = await wholesomeMemes.random();
       return await postRef.populate();
@@ -49,9 +26,4 @@ Future<Submission> getWholesomeMeme() async {
       print ('Unable to get wholesome meme. qb sad with error: $e');
     }
   }
-
-String getMemeSource(Submission post) {
-  return post.url.toString();
-}
-
 }
