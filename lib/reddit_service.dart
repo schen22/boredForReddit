9f506af07dfdbe16771dfc7c90f0abcdb6ab518c
schen22 <sarahchen22@gmail.com>
2019-06-06 23:19:47 -0700

    add try catch in reddit service

--- lib/reddit_service.dart
+++ lib/reddit_service.dart
@@ -5,25 +5,39 @@ import 'generated/post.pb.dart' as RedditData;
 
 import 'credentials.dart';
 
+Future<void> main() async {
+  RedditService service = RedditService();
+  Submission post = await service.getWholesomeMeme();
+  String title = post.title;
+  print ('title = $title');
+  String url = post.url.toString();
+  print ('url = $url'); 
+}
+
 class RedditService {
   Future<Submission> getWholesomeMeme() async {
     Credentials c = new Credentials();
-    // Create the `Reddit` instance and authenticated
-    Reddit reddit = await Reddit.createScriptInstance(
-      clientId: c.clientId,
-      clientSecret: c.clientSecret,
-      userAgent: c.userAgent,
-      username: c.username,
-      password: c.password,
-    );
-    
-    var wholesomeMemes = reddit.subreddit('wholesomememes');
     try {
-      SubmissionRef postRef = await wholesomeMemes.random();
-      return await postRef.populate();
+      // Create the `Reddit` instance and authenticated
+      Reddit reddit = await Reddit.createScriptInstance(
+        clientId: c.clientId,
+        clientSecret: c.clientSecret,
+        userAgent: c.userAgent,
+        username: c.username,
+        password: c.password,
+      );
+  
+      var wholesomeMemes = reddit.subreddit('wholesomememes');
+      try {
+        SubmissionRef postRef = await wholesomeMemes.random();
+        return await postRef.populate();
+      } catch (e) {
+        var error = e.toString();
+        print ('Unable to get wholesome meme. qb sad with error: $e');
+        return e;
+      }
     } catch (e) {
-      var error = e.toString();
-      print ('Unable to get wholesome meme. qb sad with error: $e');
+      print ('Error authenticating: ${e.toString()}');
     }
   }
 }
